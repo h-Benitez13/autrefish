8147cd9cca5da680620e84960f7ccdf9e74db61b
h-Benitez13 <hbenitez3711@gmail.com>
2019-05-14 14:24:04 -0400

    pushing card changes with images

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -21,7 +21,7 @@ export const NavContainer = ({children}) => {
 
 export const CardContainer = ({children})=> {
     return(
-        <div className="uk-section uk-section-default uk-section-small" style={{"height": "950px"}}>
+        <div className="uk-section uk-section-default uk-section-small" style={{"height": "1100px"}}>
             <div className="uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid="masonry: true" uk-scrollspy="target: div> .uk-card;cls: uk-animation-fade; delay: 500; repeat: true">
             {children}
             </div>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -10,63 +10,126 @@ function Landing() {
             </NavContainer>
 
             <CardContainer>
+                {/* first grid */}
                 <div>
-                    <div className="uk-card uk-card-secondary uk-flex uk-flex-center" style={{ "height": "450px", "backgroundImage":"url('Assets/Card/cassets.jpeg')" }} uk-scrollspy-class="uk-animation-slide-top">
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center"  uk-scrollspy-class="uk-animation-slide-top">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/cassets.jpeg" alt="" width="300px"/>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-flex uk-flex-center uk-flex-middle" style={{ "height": "300px", "backgroundImage": "url('Assets/Card/converse.jpeg')", "backgroundSize": "inherit", "backgroundRepeat": "repeat" }} uk-scrollspy-class="uk-animation-slide-bottom">
                         
                     </div>
                 </div>
                 <div>
-                    <div className="uk-card uk-card-secondary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "230px" }} uk-scrollspy-class="uk-animation-slide-bottom">
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle"  uk-scrollspy-class="uk-animation-slide-right">
                         <div className="uk-media-center">
-                            <img src="Assets/Card/converse.jpeg" alt="" />
+                            <img src="Assets/Card/dogtown.jpeg" alt="" width="300px"/>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div className="uk-card uk-card-primary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "450px" }} uk-scrollspy-class="uk-animation-slide-right">
-                        <div className="uk-media-center">
-                            <img src="Assets/Card/dogtown.jpeg" alt="" />
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div className="uk-card uk-card-primary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "600px" }} uk-scrollspy-class="uk-animation-scale-up">
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle"  uk-scrollspy-class="uk-animation-scale-up">
                         <div className="uk-media-center">
                             <img src="Assets/Card/paak.jpg" alt="" />
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div className="uk-card uk-card-secondary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "320px" }} >This is the middle</div>
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle" style={{"height": "300px"}}  > AutreFish</div>
                 </div>
                 <div>
-                    <div className="uk-card uk-card-secondary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "270px" }} uk-scrollspy-class="uk-animation-scale-down">
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle"  uk-scrollspy-class="uk-animation-scale-down">
                         <div className="uk-media-center">
                             <img src="Assets/Card/wonderer.jpeg" alt="" />
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div className="uk-card uk-card-primary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "400px" }} uk-scrollspy-class="uk-animation-slide-bottom">
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle"  uk-scrollspy-class="uk-animation-slide-bottom">
                         <div className="uk-media-center">
-                            <img src="Assets/Card/cassets.jpeg" alt="" />
+                            <img src="Assets/Card/art.gif" alt="" width="300px" />
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div className="uk-card uk-card-secondary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "200px" }} uk-scrollspy-class="uk-animation-slide-top">
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle"  uk-scrollspy-class="uk-animation-slide-top">
                         <div className="uk-media-center">
-                            <img src="Assets/Card/cassets.jpeg" alt="" />
+                            <img src="Assets/Card/sit.jpg" alt="" />
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div className="uk-card uk-card-primary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "140px" }} uk-scrollspy-class="uk-animation-slide-right">
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle"  uk-scrollspy-class="uk-animation-slide-right">
                         <div className="uk-media-center">
-                            <img src="Assets/Card/cassets.jpeg" alt="" />
+                            <img src="Assets/Card/gambino.gif" alt="" />
                         </div>
                     </div>
                 </div>
+                <div>
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle"  uk-scrollspy-class="uk-animation-scale-up">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/disclosure.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            {/* second grid */}
+                <div>
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center"  uk-scrollspy-class="uk-animation-slide-top">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/vans.jpg" alt="" width="300px"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle"  uk-scrollspy-class="uk-animation-slide-right">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/lonely-game.gif" alt="" width="600px"/>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle" style={{"height":"300px","backgroundImage": "url('Assets/Card/space.gif')","backgroundSize":"cover","backgroundRepeat":"no-repeat"}}  uk-scrollspy-class="uk-animation-scale-up">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/movement.gif" alt="" width="100px"/>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle" style={{"height": "300px"}}  >This is the middle</div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle"  uk-scrollspy-class="uk-animation-scale-down">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/kawhi.jpg" alt="" width="400px" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle"  uk-scrollspy-class="uk-animation-slide-bottom">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/records.jpg" alt="" width="500px" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle"  >
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/work.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-muted uk-flex uk-flex-center uk-flex-middle"  uk-scrollspy-class="uk-animation-slide-right">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/solange.gif" alt="" width="500px" />
+                        </div>
+                    </div>
+                </div>
+                
             </CardContainer>
         </div>
     )
