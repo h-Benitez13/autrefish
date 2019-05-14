7e40e035e1bd4f9a7e9a250f3ea9411134b27b9c
h-Benitez13 <hbenitez3711@gmail.com>
2019-05-14 11:52:09 -0400

    masonry grid, cards with images

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -4,10 +4,27 @@ import React from "react";
 
 export const Container = ({children}) => {
     return(
-        
         <div className="uk-background-cover uk-panel uk-flex uk-height-large uk-position-relative"  >
             {children}
         </div>
         
+    )
+}
+
+export const NavContainer = ({children}) => {
+    return(
+        <div className="uk-container uk-container-small uk-margin-large-bottom">
+            {children}
+        </div>
+    )
+}
+
+export const CardContainer = ({children})=> {
+    return(
+        <div className="uk-section uk-section-default uk-section-small" style={{"height": "950px"}}>
+            <div className="uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid="masonry: true" uk-scrollspy="target: div> .uk-card;cls: uk-animation-fade; delay: 500; repeat: true">
+            {children}
+            </div>
+        </div>
     )
 }
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,20 +1,74 @@
 import React from "react";
-import {Navbar} from "../components/Navbar";
-import { Container} from "../components/Container";
+import { Navbar } from "../components/Navbar";
+import { CardContainer, NavContainer } from "../components/Container";
 
 function Landing() {
-    return(
-        <Container>
-            <Navbar />
-            <div className="uk-container uk-position-center">
-                <h1 className="uk-heading-large uk-dark" uk-scrollspy="cls: uk-animation-slide-left; delay: 800; repeat: true">
-                    Autre
-                </h1>
-                <h1 className="uk-heading-large uk-dark" uk-scrollspy="cls: uk-animation-slide-right; delay: 800; repeat: true">
-                    Fish
-                </h1>
-            </div>
-        </Container>          
+    return (
+        <div>
+            <NavContainer>
+                <Navbar />
+            </NavContainer>
+
+            <CardContainer>
+                <div>
+                    <div className="uk-card uk-card-secondary uk-flex uk-flex-center" style={{ "height": "450px", "backgroundImage":"url('Assets/Card/cassets.jpeg')" }} uk-scrollspy-class="uk-animation-slide-top">
+                        
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-secondary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "230px" }} uk-scrollspy-class="uk-animation-slide-bottom">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/converse.jpeg" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-primary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "450px" }} uk-scrollspy-class="uk-animation-slide-right">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/dogtown.jpeg" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-primary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "600px" }} uk-scrollspy-class="uk-animation-scale-up">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/paak.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-secondary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "320px" }} >This is the middle</div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-secondary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "270px" }} uk-scrollspy-class="uk-animation-scale-down">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/wonderer.jpeg" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-primary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "400px" }} uk-scrollspy-class="uk-animation-slide-bottom">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/cassets.jpeg" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-secondary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "200px" }} uk-scrollspy-class="uk-animation-slide-top">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/cassets.jpeg" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="uk-card uk-card-primary uk-flex uk-flex-center uk-flex-middle" style={{ "height": "140px" }} uk-scrollspy-class="uk-animation-slide-right">
+                        <div className="uk-media-center">
+                            <img src="Assets/Card/cassets.jpeg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </CardContainer>
+        </div>
     )
 }
 export default Landing;
