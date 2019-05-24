d7a9451447deb2e63efe9744d3c03c6cdecf426f
h-Benitez13 <hbenitez3711@gmail.com>
2019-05-24 12:47:38 -0700

    contact issues

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,16 +3,15 @@ import React from "react";
 const contactImg = {
   backgroundImage: 'url("Assets/Contact/scuba.png")',
   backgroundRepeat: "no-repeat",
-  backgroundSize: "initial",
-  backgroundPosition: "bottom right",
-  height: "1050px",
-  backgroundColor: "rgb(107,146,174)"
+  backgroundSize: "inherit",
+  backgroundPosition: " center",
+  height: "800px"
 };
 
 export const Container = ({ children }) => {
   return (
     <div
-      className="uk-background-container uk-panel uk-flex uk-height-small uk-position-relative"
+      className="uk-container uk-container-large uk-panel uk-flex uk-height-small uk-position-relative"
       style={contactImg}
     >
       {children}
@@ -22,8 +21,11 @@ export const Container = ({ children }) => {
 
 export const Input = ({ children }) => {
   return (
-    <div className="uk-container uk-container-large uk-position-medium uk-position-center">
-      <form className="uk-form-horizontal uk-grid-large" uk-grid>
+    <div className="uk-container uk-container-medium uk-position-medium uk-position-left uk-margin-large-top">
+      <form
+        className="uk-form-horizontal uk-grid-medium uk-card uk-card-large uk-card-secondary"
+        uk-grid
+      >
         {children}
       </form>
     </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
   return (
     <Layout>
       <Container>
-        <div className="uk-container uk-position-small uk-position-center-left">
+        <div className="uk-container uk-position-small uk-position-center-right">
           <h1
             className="uk-heading-large uk-light"
             uk-scrollspy="cls: uk-animation-fade; delay: 500; repeat: true"
@@ -14,76 +14,73 @@ function Contact() {
             Contact
           </h1>
         </div>
-        <Input>
-          {/* first name */}
-          <div className="uk-margin-medium">
-            <div className=" uk-inline">
-              <label
-                className="uk-form-label uk-light"
-                for="form-stacked-first"
-              >
-                <h3>First Name</h3>
-              </label>
-
-              <input
-                id="form-stacked-first"
-                className="uk-input uk-form-input-width-1-2 uk-form-large uk-form-blank"
-                type="text"
-              />
-            </div>
-            {/* last name */}
-            <div className=" uk-inline">
-              <label className="uk-form-label uk-light" for="form-stacked-last">
-                <h3>Last Name</h3>
-              </label>
-
-              <input
-                id="form-stacked-last"
-                className="uk-input uk-form-input-width-1-2 uk-form-large uk-form-blank"
-                type="text"
-              />
-            </div>
-          </div>
-          {/* email */}
-          <div className="uk-margin-medium">
-            <label className="uk-form-label uk-light" for="form-label-email">
-              <h3>Email</h3>
+      </Container>
+      <Input>
+        {/* first name */}
+        <div className="uk-margin-top">
+          <div className=" uk-inline">
+            <label className="uk-form-label uk-light" for="form-stacked-first">
+              <h3>First Name</h3>
             </label>
+
             <input
-              className="uk-input uk-form-input-width-1-2 uk-form-large uk-form-blank"
-              type="email"
+              id="form-stacked-first"
+              className="uk-input uk-form-input-width-1-2 uk-form-large uk-form-blank uk-margin-right"
+              type="text"
             />
           </div>
-          {/* subject */}
-          <div className="uk-margin">
-            <label className="uk-form-label uk-light" for="form-stacked-text">
-              <h3>Subject</h3>
+          {/* last name */}
+          <div className=" uk-inline">
+            <label className="uk-form-label uk-light" for="form-stacked-last">
+              <h3>Last Name</h3>
             </label>
+
             <input
+              id="form-stacked-last"
               className="uk-input uk-form-input-width-1-2 uk-form-large uk-form-blank"
               type="text"
-              placeholder="Project for Hire!"
             />
           </div>
-          {/* text-area */}
-          <div className="uk-margin">
-            <lable className="uk-form-label uk-light" for="uk-textarea">
-              <h3>Message</h3>
-            </lable>
-            <textarea
-              className="uk-textarea uk-form-blank"
-              rows="5"
-              placeholder="Hi! I loved the website and projects, would love to collab/hire AutreFish"
-            />
-          </div>
-          {/* submit button */}
-          <div className="uk-margin ">
-            <button class="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom uk-light">
-              Button
-            </button>
-          </div>
-        </Input>
-      </Container>
+        </div>
+        {/* email */}
+        <div className="uk-margin-medium">
+          <label className="uk-form-label uk-light" for="form-label-email">
+            <h3>Email</h3>
+          </label>
+          <input
+            className="uk-input uk-form-input-width-1-2 uk-form-large uk-form-blank"
+            type="email"
+          />
+        </div>
+        {/* subject */}
+        <div className="uk-margin">
+          <label className="uk-form-label uk-light" for="form-stacked-text">
+            <h3>Subject</h3>
+          </label>
+          <input
+            className="uk-input uk-form-input-width-1-2 uk-form-large uk-form-blank"
+            type="text"
+            placeholder="Project for Hire!"
+          />
+        </div>
+        {/* text-area */}
+        <div className="uk-margin">
+          <lable className="uk-form-label uk-light" for="uk-textarea">
+            <h3>Message</h3>
+          </lable>
+          <textarea
+            className="uk-textarea uk-form-blank"
+            rows="5"
+            placeholder="Hi! I loved the website and projects, would love to collab/hire AutreFish"
+          />
+        </div>
+        {/* submit button */}
+        <div className="uk-margin ">
+          <button class="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom uk-light">
+            Button
+          </button>
+        </div>
+      </Input>
     </Layout>
   );
 }
