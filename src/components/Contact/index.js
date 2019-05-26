666ad64cc87578700ec02c4233bacf0c2b98a297
h-Benitez13 <hbenitez3711@gmail.com>
2019-05-26 14:55:24 -0700

    contact page base

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,33 +1,19 @@
 import React from "react";
 
-const contactImg = {
-  backgroundImage: 'url("Assets/Contact/scuba.png")',
-  backgroundRepeat: "no-repeat",
-  backgroundSize: "inherit",
-  backgroundPosition: " center",
-  height: "800px"
-};
-
 export const Container = ({ children }) => {
   return (
-    <div
-      className="uk-container uk-container-large uk-panel uk-flex uk-height-small uk-position-relative"
-      style={contactImg}
-    >
-      {children}
+    <div className="uk-section uk-section-default ">
+      <div className="uk-container uk-container-small">{children}</div>
     </div>
   );
 };
 
 export const Input = ({ children }) => {
   return (
-    <div className="uk-container uk-container-medium uk-position-medium uk-position-left uk-margin-large-top">
-      <form
-        className="uk-form-horizontal uk-grid-medium uk-card uk-card-large uk-card-secondary"
-        uk-grid
-      >
-        {children}
-      </form>
+    <div className="uk-container uk-container-medium ">
+      <div className="uk-card uk-card-small ">
+        <form>{children}</form>
+      </div>
     </div>
   );
 };
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,81 +6,87 @@ function Contact() {
   return (
     <Layout>
       <Container>
-        <div className="uk-container uk-position-small uk-position-center-right">
-          <h1
-            className="uk-heading-large uk-light"
-            uk-scrollspy="cls: uk-animation-fade; delay: 500; repeat: true"
-          >
-            Contact
-          </h1>
-        </div>
-      </Container>
-      <Input>
-        {/* first name */}
-        <div className="uk-margin-top">
+        <h1
+          className="uk-heading-large "
+          uk-scrollspy="cls: uk-animation-fade; delay: 500; repeat: true"
+        >
+          Contact
+        </h1>
+        <Input>
+          {/* first name */}
+
           <div className=" uk-inline">
-            <label className="uk-form-label uk-light" for="form-stacked-first">
-              <h3>First Name</h3>
+            <label className="uk-form-label " for="form-stacked-first">
+              <h4>First Name</h4>
             </label>
 
             <input
               id="form-stacked-first"
-              className="uk-input uk-form-input-width-1-2 uk-form-large uk-form-blank uk-margin-right"
+              className="uk-input uk-form-input-width-small uk-form-medium "
               type="text"
             />
           </div>
           {/* last name */}
-          <div className=" uk-inline">
-            <label className="uk-form-label uk-light" for="form-stacked-last">
-              <h3>Last Name</h3>
+          <div className=" uk-inline uk-margin-left">
+            <label className="uk-form-label " for="form-stacked-last">
+              <h4>Last Name</h4>
             </label>
 
             <input
               id="form-stacked-last"
-              className="uk-input uk-form-input-width-1-2 uk-form-large uk-form-blank"
+              className="uk-input uk-form-input-width-medium uk-form-medium"
               type="text"
             />
           </div>
-        </div>
-        {/* email */}
-        <div className="uk-margin-medium">
-          <label className="uk-form-label uk-light" for="form-label-email">
-            <h3>Email</h3>
-          </label>
-          <input
-            className="uk-input uk-form-input-width-1-2 uk-form-large uk-form-blank"
-            type="email"
-          />
-        </div>
-        {/* subject */}
-        <div className="uk-margin">
-          <label className="uk-form-label uk-light" for="form-stacked-text">
-            <h3>Subject</h3>
-          </label>
-          <input
-            className="uk-input uk-form-input-width-1-2 uk-form-large uk-form-blank"
-            type="text"
-            placeholder="Project for Hire!"
-          />
-        </div>
-        {/* text-area */}
-        <div className="uk-margin">
-          <lable className="uk-form-label uk-light" for="uk-textarea">
-            <h3>Message</h3>
-          </lable>
-          <textarea
-            className="uk-textarea uk-form-blank"
-            rows="5"
-            placeholder="Hi! I loved the website and projects, would love to collab/hire AutreFish"
-          />
-        </div>
-        {/* submit button */}
-        <div className="uk-margin ">
-          <button class="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom uk-light">
-            Button
-          </button>
-        </div>
-      </Input>
+
+          {/* email */}
+          <div className="uk-margin">
+            <label className="uk-form-label " for="form-label-email">
+              <h4>Email</h4>
+            </label>
+            <input
+              id="form-label-email"
+              className="uk-input uk-form-input-width-1-3 uk-form-medium"
+              type="email"
+            />
+          </div>
+
+          {/* subject */}
+          <div>
+            <label className="uk-form-label " for="form-stacked-text">
+              <h4>Subject</h4>
+            </label>
+            <input
+              id="form-stacked-text"
+              className="uk-input uk-form-input-width-medium uk-form-medium"
+              type="text"
+              placeholder="Project for Hire!"
+            />
+          </div>
+
+          {/* text-area */}
+          <div className="uk-margin">
+            <lable className="uk-form-label " for="uk-textarea">
+              <h4>Message</h4>
+            </lable>
+            <textarea
+              className="uk-textarea"
+              rows="5"
+              placeholder="Hi! I loved the website and projects, would love to collab/hire AutreFish"
+            />
+          </div>
+
+          {/* submit button */}
+          <div className="uk-margin ">
+            <a
+              href="mailto:weare@autrefish.com"
+              className="uk-button uk-button-default uk-width-1-1 uk-margin-small-bottom"
+            >
+              Button
+            </a>
+          </div>
+        </Input>
+      </Container>
     </Layout>
   );
 }
