17942a6b5cd47a27d39dd0f2a0d97f6bfcba6979
h-Benitez13 <hbenitez3711@gmail.com>
2019-06-20 15:26:24 -0400

    404 w/ lucky

--- src/components/Contact/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Container = ({ children }) => {
-  return (
-    <div className="uk-section uk-section-small" style={{ minHeight: "100vh" }}>
-      {children}
-    </div>
-  );
-};
--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NoMatch() {
   return (
-    <div>
-      <h1>404 Page Not Found</h1>
-      <h1>
-        <span role="img" aria-label="Face With Rolling Eyes Emoji">
-          🙄
-        </span>
-      </h1>
+    <div id="noMatch">
+      <div className="uk-container uk-position-center uk-text-center">
+        <h1 className="uk-heading-primary uk-light ">404 Page Not Found</h1>
+        <img
+          src="/Assets/Landing/lucky_404.jpg"
+          alt="Lucky"
+          width="300px"
+          style={{ borderRadius: "10px" }}
+        />
+
+        <h3 className=" uk-light">
+          Everybody makes mistakes (everybody has those days). Click{" "}
+          <Link to="/">here</Link> to head back!
+        </h3>
+      </div>
     </div>
   );
 }
