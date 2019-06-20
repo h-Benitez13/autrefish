9d2c286889ace3a9a385d959e294fbf6412644c0
h-Benitez13 <hbenitez3711@gmail.com>
2019-06-20 15:04:13 -0400

    landing space

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,13 +1,5 @@
 import React from "react";
 
-export const Container = ({ children }) => {
-  return (
-    <div className="uk-background-cover uk-panel uk-flex uk-height-large uk-position-relative">
-      {children}
-    </div>
-  );
-};
-
 export const NavContainer = ({ children }) => {
   return (
     <div className="uk-container uk-container-large uk-margin-large-bottom">
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -11,9 +11,7 @@ function Landing() {
           <div id="card1" className="uk-card uk-position-center ">
             <div className="uk-inline-clip uk-transition-toggle" tabIndex="0">
               <img src="/Assets/Landing/pro2.jpg" alt="Cojo - Block boy" />
-              <div className="uk-overlay uk-light uk-position-top">
-                <h4>Cojo - Block boy</h4>
-              </div>
+              <div className="uk-overlay uk-light uk-position-top" />
               <iframe
                 className="uk-transition-fade uk-position-cover"
                 id="youtubeVid"
@@ -39,20 +37,18 @@ function Landing() {
         {/* Grid 2 */}
 
         <div>
-          <div id="card2" className="uk-card uk-card-muted uk-flex">
+          <div id="card2" className="uk-card uk-flex ">
             <div className="uk-inline-clip uk-transition-toggle" tabIndex="0">
               <img src="/Assets/Landing/pro4.jpg" alt="BR-STOL x new era" />
-              <div className="uk-overlay uk-light uk-position-top">
-                <h4>BR-STOL x New Era</h4>
-              </div>
+              <div className="uk-overlay uk-light uk-position-top" />
 
               <iframe
                 id="newEra"
                 title="vimeo- BR-STOL x new era"
                 className="uk-transition-fade uk-position-cover"
                 src="https://player.vimeo.com/video/325809855?title=0&byline=0"
-                width="569"
-                height="300"
+                width="664"
+                height="350"
                 frameBorder="0"
                 allow="autoplay; fullscreen"
                 allowFullScreen
@@ -67,15 +63,13 @@ function Landing() {
         {/* Grid 3 */}
 
         <div>
-          <div id="card3" className="uk-card uk-card-muted uk-flex">
+          <div id="card3" className="uk-card ">
             <div
               className="uk-inline-clip uk-transition-toggle uk-position-cover"
               tabIndex="0"
             >
               <img src="/Assets/Landing/pro1.jpg" alt="nig." />
-              <div className="uk-overlay uk-light uk-position-top">
-                <h4>nig.</h4>
-              </div>
+              <div className="uk-overlay uk-light uk-position-top" />
 
               <iframe
                 id="nig"
@@ -92,15 +86,13 @@ function Landing() {
           </div>
         </div>
         <div>
-          <div id="card4" className="uk-card uk-card-muted uk-flex">
+          <div id="card4" className="uk-card ">
             <div
               className="uk-inline-clip uk-transition-toggle uk-position-cover"
               tabIndex="0"
             >
               <img src="/Assets/Landing/pro3.png" alt="BR-STOL at Coachella" />
-              <div className="uk-overlay uk-light uk-position-top">
-                <h4>BR–STOL at Coachella</h4>
-              </div>
+              <div className="uk-overlay uk-light uk-position-top" />
               <iframe
                 id="coachella"
                 title="BR-STOL at Coachella"
@@ -119,15 +111,13 @@ function Landing() {
         {/* Grid 4 */}
 
         <div>
-          <div id="card5" className="uk-card uk-card-muted uk-flex">
+          <div id="card5" className="uk-card ">
             <div
               className="uk-inline-clip uk-transition-toggle uk-position-cover"
               tabIndex="0"
             >
               <img src="/Assets/Landing/pro5.png" alt="BR-STOL X NY Sunshine" />
-              <div className="uk-overlay uk-light uk-position-top">
-                <h4>BR-STOL X NY Sunshine</h4>
-              </div>
+              <div className="uk-overlay uk-light uk-position-top" />
 
               <iframe
                 id="NY"
@@ -157,15 +147,13 @@ function Landing() {
           />
         </div>
         <div>
-          <div id="card6" className="uk-card uk-card-muted uk-flex">
+          <div id="card6" className="uk-card ">
             <div
               className="uk-inline-clip uk-transition-toggle uk-position-cover"
               tabIndex="0"
             >
               <img src="/Assets/Landing/pro6.png" alt="BR-STOL Memoriam Gym" />
-              <div className="uk-overlay uk-light uk-position-top">
-                <h4>BR-STOL Memoriam Gym</h4>
-              </div>
+              <div className="uk-overlay uk-light uk-position-top" />
               <iframe
                 title="BR-STOL Memoriam Gym"
                 className="uk-transition-fade uk-position-cover"
